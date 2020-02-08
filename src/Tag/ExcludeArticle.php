e52721f0d56659d18e48a3e5336e28f046755be1
pwirth <wirth@hallowelt.biz>
2020-02-08 11:56:26 +0100

    Use tag integration for tag excludearticle
    ERM:17986
    [3.2]
    
    Change-Id: I1b39a26ecfe76516b75a3b7e977060048c98f870

---
+++ src/Tag/ExcludeArticle.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace BlueSpice\UniversalExport\Tag;
+
+use BlueSpice\Tag\MarkerType;
+use BlueSpice\Tag\MarkerType\NoWiki;
+use BlueSpice\Tag\Tag;
+use Parser;
+use PPFrame;
+
+class ExcludeArticle extends Tag {
+
+	/**
+	 *
+	 * @return bool
+	 */
+	public function needsParsedInput() {
+		return false;
+	}
+
+	/**
+	 *
+	 * @return bool
+	 */
+	public function needsParseArgs() {
+		return false;
+	}
+
+	/**
+	 *
+	 * @return MarkerType
+	 */
+	public function getMarkerType() {
+		return new NoWiki();
+	}
+
+	/**
+	 *
+	 * @param string $processedInput
+	 * @param array $processedArgs
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 * @return ExcludeArticleHandler
+	 */
+	public function getHandler( $processedInput, array $processedArgs, Parser $parser,
+		PPFrame $frame ) {
+		return new ExcludeArticleHandler(
+			$processedInput,
+			$processedArgs,
+			$parser,
+			$frame
+		);
+	}
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getTagNames() {
+		return [
+			'pdfexcludepage',
+			'universalexport:excludearticle',
+			'bs:universalexport:excludearticle',
+			'bs:ueexcludearticle'
+		];
+	}
+
+}
