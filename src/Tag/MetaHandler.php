3c015320baae5782183cd971e39620f8b953f504
pwirth <wirth@hallowelt.biz>
2020-02-11 16:55:10 +0100

    Use tag integration for tag universalexport:meta
    ERM:17986
    [3.2]
    
    Change-Id: I0fcce75e940308c2aedae116825f1f2a41b6abfd

---
+++ src/Tag/MetaHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BlueSpice\UniversalExport\Tag;
+
+use BlueSpice\Tag\Handler;
+use FormatJson;
+use Html;
+
+class MetaHandler extends Handler {
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function handle() {
+		$this->parser->getOutput()->setProperty( 'bs-tag-universalexport-meta', 1 );
+		$this->parser->getOutput()->setProperty(
+			'bs-universalexport-meta',
+			FormatJson::encode( $this->processedArgs )
+		);
+		$attribs = array_merge(
+			$this->processedArgs,
+			[ 'class' => 'bs-universalexport-meta' ]
+		);
+
+		return Html::element( 'div', $attribs );
+	}
+}
