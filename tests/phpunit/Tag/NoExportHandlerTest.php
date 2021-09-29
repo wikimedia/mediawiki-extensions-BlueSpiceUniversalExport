fc83708ddc4df9847c5c0d9c770d4f050f22cbd0
rvogel <vogel@hallowelt.biz>
2021-09-29 17:16:48 +0200

    Fix missing content in `<bs:uenoexport>`
    Also add inline/blocklevel detection
    
    Depends-On: Ib976a11e4bb6b0f4306f862173583f3c7de4abb3
    
    ERM22997
    
    [3.2.x]
    
    Change-Id: Iebb3d6e70f79132a7c63ff0588649f8953a3377c

---
+++ tests/phpunit/Tag/NoExportHandlerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace BlueSpice\UniversalExport\Tests\Tag;
+
+use BlueSpice\UniversalExport\Tag\NoExportHandler;
+use Parser;
+use ParserOutput;
+use PHPUnit\Framework\TestCase;
+use PPFrame;
+
+class NoExportHandlerTest extends TestCase {
+
+	/**
+	 * @param string $input
+	 * @param string $expectedContainerEl
+	 * @covers BlueSpice\UniversalExport\Tag\NoExportHandler::handle
+	 * @dataProvider provideTestHandleData
+	 */
+	public function testHandle( $input, $expectedContainerEl ) {
+		$mockParser = $this->createMock( Parser::class );
+		$mockParserOutput = $this->createMock( ParserOutput::class );
+		$mockParser->method( 'getOutput' )->willReturn( $mockParserOutput );
+		$mockFrame = $this->createMock( PPFrame::class );
+		$handler = new NoExportHandler( $input, [], $mockParser, $mockFrame );
+		$output = $handler->handle();
+
+		$this->assertStringStartsWith( "<$expectedContainerEl", $output );
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function provideTestHandleData() {
+		return [
+			'simple-inline' => [ 'Lorem ipsum', 'span' ],
+			'complex-inline' => [ 'Lorem [[ipsum]], {{Some}} \'\'Text\'\' and ***, #', 'span' ],
+			'simple-wikitext-paragraph' => [ "Lorem\n\nipsum", 'div' ],
+			'complex-blocklevel-01' => [ 'Lorem <div class="red">ipsum</div>', 'div' ],
+			'complex-blocklevel-02' => [ "Lorem \n:indent me", 'div' ],
+			'complex-blocklevel-03' => [ "Lorem \n:indent me", 'div' ],
+			'complex-blocklevel-04' => [ "Lorem \n*list me", 'div' ],
+			'complex-blocklevel-05' => [ "Lorem \n preformat me", 'div' ],
+		];
+	}
+}
