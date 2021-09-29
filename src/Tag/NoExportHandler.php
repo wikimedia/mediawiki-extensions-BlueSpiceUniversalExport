fc8fbacacc95b621e6d8e1c2c3dd2550163ba284
rvogel <vogel@hallowelt.biz>
2021-09-29 17:16:48 +0200

    Fix missing content in `<bs:uenoexport>`
    Also add inline/blocklevel detection
    
    Depends-On: Ib976a11e4bb6b0f4306f862173583f3c7de4abb3
    
    ERM22997
    
    [3.2.x]
    
    Change-Id: Iebb3d6e70f79132a7c63ff0588649f8953a3377c

--- src/Tag/NoExportHandler.php
+++ src/Tag/NoExportHandler.php
@@ -14,11 +14,68 @@ class NoExportHandler extends Handler {
 	 */
 	public function handle() {
 		$this->parser->getOutput()->setProperty( 'bs-tag-universalexport-exclude', 1 );
+		$elementName = $this->isBlocklevelInput() ? 'div' : 'span';
 		$msg = Message::newFromKey( 'bs-universalexport-tag-exclude-text' )->plain();
 		return Html::rawElement(
-			'div',
+			$elementName,
 			[ 'class' => 'bs-universalexport-exportexclude', 'title' => $msg ],
-			$this->parser->recursiveTagParse( $this->processedInput )
+			$this->processedInput
 		);
+	}
+
+	private function isBlocklevelInput() {
+		// Wikitext parapraphs
+		if ( strpos( $this->processedInput, "\n\n" ) !== false ) {
+			return true;
+		}
+
+		// Wikitext unordered list
+		if ( strpos( $this->processedInput, "\n*" ) !== false ) {
+			return true;
+		}
+
+		// Wikitext ordered list
+		if ( strpos( $this->processedInput, "\n#" ) !== false ) {
+			return true;
+		}
+
+		// Wikitext definition list
+		if ( strpos( $this->processedInput, "\n;" ) !== false ) {
+			return true;
+		}
+
+		// Wikitext text indention
+		if ( strpos( $this->processedInput, "\n:" ) !== false ) {
+			return true;
+		}
+
+		// Wikitext table
+		if ( strpos( $this->processedInput, "\n{|" ) !== false ) {
+			return true;
+		}
+
+		// Wikitext preformatted text
+		if ( strpos( $this->processedInput, "\n " ) !== false ) {
+			return true;
+		}
+
+		// Wikitext heading
+		if ( strpos( $this->processedInput, "\n=" ) !== false ) {
+			return true;
+		}
+
+		// HTML div
+		if ( preg_match( "#<div.*?>#", $this->processedInput ) === 1 ) {
+			return true;
+		}
+
+		// HTML heading
+		if ( preg_match( "#<h\d.*?>#", $this->processedInput ) === 1 ) {
+			return true;
+		}
+
+		// More analysis required? Actually we'd probably need to test on the parsed content!
+
+		return false;
 	}
 }
