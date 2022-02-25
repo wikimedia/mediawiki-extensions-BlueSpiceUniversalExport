bf20aa6d24706251c0a79a5caca6de4f98d7207b
d.savuljesku <d.savuljesku@gmail.com>
2022-02-25 13:26:32 +0100

    Fix no export matching for SMW inline query results
    [4.1.x]
    
    ERM 27682
    
    Change-Id: Ib40fc17e3280f365a74a09321b01e4983c48454e

--- src/Tag/NoExportHandler.php
+++ src/Tag/NoExportHandler.php
@@ -14,68 +14,18 @@ class NoExportHandler extends Handler {
 	 */
 	public function handle() {
 		$this->parser->getOutput()->setProperty( 'bs-tag-universalexport-exclude', 1 );
-		$elementName = $this->isBlocklevelInput() ? 'div' : 'span';
 		$msg = Message::newFromKey( 'bs-universalexport-tag-exclude-text' )->plain();
+		$this->processedInput = $this->parser->recursiveTagParseFully( $this->processedInput );
+		$matches = [];
+
+		preg_match( '/^<p>(.*?)\\n<\/p>$/', $this->processedInput, $matches );
+		$inline = !empty( $matches );
+		$elementName = $inline ? 'span' : 'div';
+
 		return Html::rawElement(
 			$elementName,
 			[ 'class' => 'bs-universalexport-exportexclude', 'title' => $msg ],
-			$this->parser->recursiveTagParseFully( $this->processedInput )
+			$inline ? $matches[1] : $this->processedInput
 		);
-	}
-
-	private function isBlocklevelInput() {
-		// Wikitext parapraphs
-		if ( strpos( $this->processedInput, "\n\n" ) !== false ) {
-			return true;
-		}
-
-		// Wikitext unordered list
-		if ( strpos( $this->processedInput, "\n*" ) !== false ) {
-			return true;
-		}
-
-		// Wikitext ordered list
-		if ( strpos( $this->processedInput, "\n#" ) !== false ) {
-			return true;
-		}
-
-		// Wikitext definition list
-		if ( strpos( $this->processedInput, "\n;" ) !== false ) {
-			return true;
-		}
-
-		// Wikitext text indention
-		if ( strpos( $this->processedInput, "\n:" ) !== false ) {
-			return true;
-		}
-
-		// Wikitext table
-		if ( strpos( $this->processedInput, "\n{|" ) !== false ) {
-			return true;
-		}
-
-		// Wikitext preformatted text
-		if ( strpos( $this->processedInput, "\n " ) !== false ) {
-			return true;
-		}
-
-		// Wikitext heading
-		if ( strpos( $this->processedInput, "\n=" ) !== false ) {
-			return true;
-		}
-
-		// HTML div
-		if ( preg_match( "#<div.*?>#", $this->processedInput ) === 1 ) {
-			return true;
-		}
-
-		// HTML heading
-		if ( preg_match( "#<h\d.*?>#", $this->processedInput ) === 1 ) {
-			return true;
-		}
-
-		// More analysis required? Actually we'd probably need to test on the parsed content!
-
-		return false;
 	}
 }
